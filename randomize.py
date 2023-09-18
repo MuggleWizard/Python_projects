5c89d0b9d95a50368bbf68744e607c1d7a43fe45
Your Name <you@example.com>
2023-09-18 13:23:38 -0500

    added cards to randomize cards

--- randomize.py
+++ randomize.py
@@ -9,7 +9,7 @@ prizes = ["a car", "$100000", "a pony", "$500000", "a castle"]
 prize_won = random.choice(prizes)
 print("You turn the wheel of fortune... It lands on a prize of", prize_won + "!! \n")
 
-cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 random.shuffle(cards)
 print("The cards are now in this order: ")
 print(cards, "\n")
