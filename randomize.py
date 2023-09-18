a1c4b407e5face7cd1ef580fa5d4a19d2f8504c2
Darlene Kartali <darlenekartali@Darlenes-MacBook-Air.local>
2023-09-18 13:14:37 -0500

    randomize.py

---
+++ randomize.py
@@ -0,0 +1,15 @@
+import random
+
+
+print("")
+pips = random.randint(1, 6)
+print("You roll the die... it lands with", pips, "pips showing. \n")
+
+prizes = ["a car", "$100000", "a pony", "$500000"]
+prize_won = random.choice(prizes)
+print("You turn the wheel of fortune... It lands on a prize of", prize_won + "!! \n")
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+random.shuffle(cards)
+print("The cards are now in this order: ")
+print(cards, "\n")
