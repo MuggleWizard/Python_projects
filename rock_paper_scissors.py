20774f5535c6c870be686ab78a8b1530d9d36e65
Your Name <you@example.com>
2025-09-03 10:46:19 -0500

    worked on while true loop

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,20 +2,36 @@ import random
 
 
 def play():
-    user = input(
-        "What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    user = user.lower()
 
-    computer = random.choice(['r', 'p', 's'])
+    while True:
+        user = input(
+            "What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+        user = user.lower()
 
-    if user == computer:
-        return "You and the computer have chosen {}. It's a tie. ".format(computer)
+        if user not in ['r', 'p', 's']:
+            print("Not a valid entry")
+            continue
+        else:
+            print("You threw {}.".format(user))
+
+        computer = random.choice(['r', 'p', 's'])
+
+        if user == computer:
+            print("You and the computer have chosen {}. It's a tie. ").format(
+                computer)
 
     # r > s, s > p, p > r
-    if is_win(user, computer):
-        return "You have chosen {} and the computer has chosen {}. You won!".format(user, computer)
+        if is_win(user, computer):
+            return "You have chosen {} and the computer has chosen {}. You won!".format(user, computer)
+        return "You have chosen {} and the computer has chosen {}. You lost :(".format(user, computer)
 
-    return "You have chosen {} and the computer has chosen {}. You lost :(".format(user, computer)
+
+def restart_game():
+    player_input = input("\n Play again? Yes or No "). lower()
+    if player_input == "no" or player_input == "No" or player_input == "n":
+        print("byeeeee")
+    elif player_input == "yes" or player_input == "Yes" or player_input == "y":
+        print("Here we go!")
 
 
 def is_win(player, opponent):
@@ -27,3 +43,13 @@ def is_win(player, opponent):
 
 
 print(play())
+print(restart_game())
+
+
+# def restart_game():
+
+#     restart_game = input("Battle again? Yes or No: ").lower()
+#     if restart_game == "no" or restart_game == "No" or restart_game == "n":
+#         #!= "n"
+#         print("Farewell valiant fighter!")
+#         # break
