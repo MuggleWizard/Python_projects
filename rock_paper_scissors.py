010eac6eb42c422bb92de37f8db4154e2eb29d9b
Your Name <you@example.com>
2025-09-11 08:20:06 -0500

    added counters for wins, ties, losses and code to calculate the win percentage

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,16 @@
 import random
 
+wins = 0
+ties = 0
+loss = 0
 games_played = 0
+
+
+def update_win_percentage(wins, games_played):
+    if games_played == 0:
+        return 0  # avoid division by zero
+    return (wins/games_played) * 100
+
 
 while True:
 
@@ -51,34 +61,51 @@ while True:
         computer = random.choice(['r', 'p', 's'])
 
         if user == computer:
-            return "You and the computer have chosen {}. It's a tie. ".format(computer)
+            global ties
+            ties += 1
+            return "You and the computer have chosen {}. It's a tie.\n ".format(computer) + (f"Tie: {ties}")
+
 
 # r > s, s > p, p > r
         if is_win(user, computer):
-            return "You have chosen {} and the computer has chosen {}. You won!\n".format(
-                user, computer)
+            return "You have chosen {} and the computer has chosen {}. You won!\n ".format(
+                user, computer) + (f"Wins: {wins}")
         return "You have chosen {} and the computer has chosen {}. You lost :(\n".format(
-            user, computer)
+            user, computer) + (f"Lost: {loss}")
 
     def is_win(player, opponent):
+        global wins
+        global loss
         # return true if the player beats the opponent
         # winning conditions: r > s , s > p , p > r
         if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+            wins += 1
             return True
+        else:
+            loss += 1
         return False
 
     print(play())
+
+    # if games_played > 0:
+    #     win_percentage = update_win_percentage(wins, games_played)
+    # print(
+    #     f"Games played: {games_played}, Wins: {wins}, Win percentage:{win_percentage:.2f}%")
+    # else:
+    #     print("NO games played yet")
 
     restart_game = input("\nPlay again? Yes or No: ").lower()
 
     match restart_game:
         case "no" | "n" | "No":
             print("Adios senor")
+            win_percentage = update_win_percentage(wins, games_played)
+            print(
+                f"Total rounds played: {games_played + 1}, Wins: {wins}, Wins percentage: {win_percentage:.2f}%")
             break
         case "yes" | "y" | "Yes":
             print("As you wish")
             games_played += 1
-            print(f"Total rounds played: {games_played}")
         case _:
             print("That's not a valid response amigo")
 
@@ -96,6 +123,6 @@ while True:
 # if__name__ == '__rock_paper_scissors__':
 #     print(play())
 
-        # if counter >= 10:
-        #     print("Counter has reached the limit! Better luck next time! \n")
-        #     break
+    # if counter >= 10:
+    #     print("Counter has reached the limit! Better luck next time! \n")
+    #     break
